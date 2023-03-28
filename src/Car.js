4c7ec53e41c0fe716b65d65db300d4fddc105206
Zulk0rne1n <ulasardaburak@outlook.com>
2023-03-28 11:16:35 +0300

    files added

---
+++ src/Car.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Car extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return ();
+    }
+}
+ 
+export default ;
